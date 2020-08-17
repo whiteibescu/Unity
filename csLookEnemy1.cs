b2e5ac8c478070b83af1314cd6d9aaa3a66edb60
성수 박 <whiteibescu@gmail.com>
2020-08-17 17:53:34 +0900

    Lecture

---
+++ csLookEnemy1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csLookEnemy1 : MonoBehaviour
+{
+    public Transform enemy;
+    private Transform viewer;
+    private Transform spPoint;
+
+    RaycastHit hit;
+    Vector3 fwd = Vector3.forward;
+    public float rayLength = 4f;
+    void Start()
+    {
+        viewer = transform.Find("/Turret/Viewer");
+        spPoint = transform.Find("/Turret/Tower/SpawnPoint");
+    }
+    void Update()
+    {
+        // viewer가 주시하고 회전각도를 동일하게 적용해준다
+        viewer.LookAt(enemy);
+        transform.rotation = viewer.rotation;
+
+        Debug.DrawRay(spPoint.position,
+            spPoint.forward * rayLength, Color.red);
+        if (Physics.Raycast(spPoint.position, fwd, out hit, rayLength))
+            Debug.Log(hit.collider.gameObject.name);
+    }
+}
