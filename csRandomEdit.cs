faaad66f4cb381461406db946a6f1ff9900cbb01
성수 박 <whiteibescu@gmail.com>
2020-08-17 17:53:45 +0900

    Lecture

---
+++ csRandomEdit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csRandomEdit : MonoBehaviour
+{
+    void Start()
+    {
+        int myVal = 0;
+        float nLimit = Mathf.Clamp(myVal, 1, 5);
+        Debug.Log("Clamps : " + nLimit);
+
+        int randomSeeds;
+        randomSeeds = (int)System.DateTime.Now.Ticks;
+        Random.InitState(randomSeeds);
+
+        int randomX = Random.Range(5, 10);
+        Debug.Log("Integer : " + randomX);
+
+        float randomY = Random.Range(5f, 10f);
+        Debug.Log("Float : " + randomY);
+    }
+}
